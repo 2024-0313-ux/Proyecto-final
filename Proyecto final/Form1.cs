210808daae383d4ec229d0356f5c25526578def8
osias <user.email.2024-0313@unad.edu.do>
2025-11-26 13:13:00 -0400

    Ya estan creados todos los formularios respecto al Menú de requerimientos Agregar archivos de proyecto.

---
+++ Proyecto final/Form1.cs
@@ -0,0 +1,25 @@
+namespace Proyecto_final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Proyecto final/Formularios/Usuarios/FrmUsuarioDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_final.Formularios.Usuarios
+{
+    public partial class FrmUsuarioDetalle : Form
+    {
+        public FrmUsuarioDetalle()
+        {
+            InitializeComponent();
+        }
+    }
+}
