e8331eef6c4cdb3b76650f91bb041bd90d7289f5
osias <user.email.2024-0313@unad.edu.do>
2025-12-02 21:17:17 -0400

    el diseño del formulario faltante lo termine

--- Proyecto final/Formularios/Reportes/FrmReporteSocio.Designer.cs
+++ Proyecto final/Formularios/Reportes/FrmReporteSocio.Designer.cs
@@ -28,12 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmReporteSocio";
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(49, 73);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 0;
+            comboBox1.Text = "Socio";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(56, 145);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 1;
+            button1.Text = "Generar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(68, 207);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(360, 225);
+            dataGridView1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(56, 445);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 34);
+            button2.TabIndex = 3;
+            button2.Text = "Exportar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(300, 445);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 34);
+            button3.TabIndex = 4;
+            button3.Text = "Cerrar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // FrmReporteSocio
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 491);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Name = "FrmReporteSocio";
+            Text = "FrmReporteSocio";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private Button button3;
     }
 }
--- Proyecto final/Formularios/Reportes/FrmReporteSocio.cs
+++ Proyecto final/Formularios/Reportes/FrmReporteSocio.cs
@@ -16,5 +16,10 @@ namespace Proyecto_final.Formularios.Reportes
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
