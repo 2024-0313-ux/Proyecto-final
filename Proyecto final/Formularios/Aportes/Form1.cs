9e802dc348910b1918b10d45748ccb33e36bd245
osias <user.email.2024-0313@unad.edu.do>
2025-12-08 19:56:18 -0400

    Agregue un boton

--- Proyecto final/Formularios/Aportes/Form1.cs
+++ Proyecto final/Formularios/Aportes/Form1.cs
@@ -16,5 +16,10 @@ namespace Proyecto_final.Formularios.Aportes
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
